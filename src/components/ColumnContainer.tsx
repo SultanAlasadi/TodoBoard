8a45d514a0292e968581966b05162273d90e3941
F.society <sultanalasadi7@gmail.com>
2024-06-07 03:39:23 +0300

    chore: Add drag and drop functionality to column containers

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,14 +1,57 @@
-import { Column } from "../Typs/typs";
+import { useSortable } from "@dnd-kit/sortable";
+import { Column, Id } from "../Typs/typs";
 import Trash from "../icons/Trash";
-
+import { CSS } from "@dnd-kit/utilities";
 interface Props {
   column: Column;
+  deleteColumn: (id: Id) => void;
 }
 
 const ColumnContainer = (props: Props) => {
-  const { column } = props;
+  const { column, deleteColumn } = props;
+
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+  });
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="
+    bg-maincolor
+    w-[350px]
+    h-[500px]
+    max-h-[500px]
+    rounded-md 
+    flex flex-col
+    opacity-40
+    border-2
+    border-rose-500
+    "
+      ></div>
+    );
+  }
   return (
     <div
+      ref={setNodeRef}
+      style={style}
       className="
     bg-maincolor
     w-[350px]
@@ -19,6 +62,8 @@ const ColumnContainer = (props: Props) => {
     "
     >
       <div
+        {...attributes}
+        {...listeners}
         className="
       bg-maincolor
       text-md 
@@ -41,7 +86,12 @@ const ColumnContainer = (props: Props) => {
           </div>
           {column.title}
         </div>
-        <button className="stroke-gray-500 hover:stroke-white rounded px-1 py-2">
+        <button
+          onClick={() => {
+            deleteColumn(column.id);
+          }}
+          className="stroke-gray-500 hover:stroke-white rounded px-1 py-2"
+        >
           <Trash />
         </button>
       </div>
--- src/components/KabanBoard.tsx
+++ src/components/KabanBoard.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
-import { Column } from "../Typs/typs";
+import { Column, Id } from "../Typs/typs";
 import ColumnContainer from "./ColumnContainer";
-
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
 const KabanBoard = () => {
   const [colums, setColums] = useState<Column[]>([]);
-  console.log(colums);
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 3,
+      },
+    })
+  );
+  const columnId = useMemo(() => colums.map((column) => column.id), [colums]);
 
   function createColum() {
     const columnToAdd: Column = {
@@ -14,26 +33,83 @@ const KabanBoard = () => {
     };
     setColums([...colums, columnToAdd]);
   }
+  function deleteColumn(id: Id) {
+    const newColums = colums.filter((column) => column.id !== id);
+    console.log("newColums", newColums);
+    setColums(newColums);
+  }
+
+  function onDragStart(event: DragStartEvent) {
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column);
+      return;
+    }
+  }
+
+  function onDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (!over) return;
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+
+    if (activeColumnId === overColumnId) return;
+
+    setColums((columns) => {
+      const activeColumnIndex = columns.findIndex(
+        (column) => column.id === activeColumnId
+      );
+
+      const overColumnIndex = columns.findIndex(
+        (column) => column.id === overColumnId
+      );
+
+      return arrayMove(columns, activeColumnIndex, overColumnIndex);
+    });
+  }
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className=" m-auto flex gap-4">
-        <div className="flex gap-2 ">
-          {colums.map((column) => (
-            <ColumnContainer key={column.id} column={column} />
-          ))}
-        </div>
-        <button
-          onClick={() => {
-            createColum();
-          }}
-          className="
+      <DndContext
+        sensors={sensors}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
+        <div className=" m-auto flex gap-4">
+          <div className="flex gap-2 ">
+            <SortableContext items={columnId}>
+              {colums.map((column) => (
+                <ColumnContainer
+                  key={column.id}
+                  column={column}
+                  deleteColumn={deleteColumn}
+                />
+              ))}{" "}
+            </SortableContext>
+          </div>
+          <button
+            onClick={() => {
+              createColum();
+            }}
+            className="
        h-[60px] w-[350px] min-w-[350px] cursor-pointer  rounded-lg bg-maincolor border-2 border-secondary
        p-4 ring-rose-500 hover:ring-2 flex gap-2  active:border-0
        "
-        >
-          <PlusIcon /> Click me
-        </button>
-      </div>
+          >
+            <PlusIcon /> Click me
+          </button>
+        </div>
+
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
+      </DndContext>
     </div>
   );
 };
